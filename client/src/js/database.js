301dc4712d530c2c0a7b86fea876d6f1c0d206a6
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-26 21:18:43 -0700

    add database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const todosDb = await openDB("jate", 1);
+  const tx = todosDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const res = await store.put({ id: 1, content: content });
+  console.log("Saved to database!");
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const todosDb = await openDB("jate", 1);
+  const tx = todosDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  return result.content;
+};
 
 initdb();
+
